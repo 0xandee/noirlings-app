1521126cf8f481d4c6f254ec0268fc8538e71b65
0xandee <34095856+0xandee@users.noreply.github.com>
2025-05-02 06:04:51 +0700

    feat: integrate Vercel Analytics into the playground
    - Added the `@vercel/analytics` package to the project dependencies to enable analytics tracking.
    - Imported and included the `Analytics` component in the `index.tsx` file to start tracking user interactions.
    - Updated the `yarn.lock` and `package.json` files to reflect the new dependency.
    - Enhanced the overall functionality of the playground by providing insights into user engagement.
    - Updated `summary.md` to document these changes and ensure accurate tracking of modifications in the codebase.

--- packages/playground/index.tsx
+++ packages/playground/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import NoirEditor from "./src/index";
-
+import { Analytics } from "@vercel/analytics/react"
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <NoirEditor
-    baseUrl={
+  <>
+    <NoirEditor
+      baseUrl={
       process.env.NODE_ENV === "development"
         ? window.location.host
         : "https://noir-playground.netlify.app"
     }
   />
+  <Analytics />
+  </>
 );
